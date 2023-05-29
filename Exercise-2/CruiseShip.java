0857d8ffd148e254d092379324ed95cd8f129ce1
Collin Hargreaves <collinj@Collins-MacBook-Air.local>
2023-05-29 15:38:40 -0700

    push

--- Exercise-2/CruiseShip.java
+++
@@ -1,9 +0,0 @@
-public class CruiseShip extends Ship {
-
-    public CruiseShip(int capacity, int length, int width, String name, int height, String departureDate,
-            String arrivalDate, String owner) {
-        super(capacity, length, width, name, height, departureDate, arrivalDate, owner);
-        //TODO Auto-generated constructor stub
-    }
-    
-}
